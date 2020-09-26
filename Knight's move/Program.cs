5c3d1b0d030c5880a2864c2d15f83ef78eab1fd5
ReanesX <miha200273@gmail.com>
2020-09-26 11:07:05 +0500

    Добавьте файлы проекта.

---
+++ Knight's move/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Knight_s_move
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+        }
+
+    }
+}
